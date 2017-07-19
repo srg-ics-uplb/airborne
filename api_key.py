9b12183c73d9c277bc96c027ff0177f1ecb846c9
ClarkAlmazan <clark.almazan@gmail.com>
2017-07-19 17:49:04 +0800

    Remove api key

--- api_key.py
+++
@@ -1,6 +0,0 @@
-"""
-	Python module that stores the Google Maps API Key
-	You can get your own from here: https://developers.google.com/maps/documentation/javascript/get-api-key
-"""
-
-api_key = '<your_api_key>'
