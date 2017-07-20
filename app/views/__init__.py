ff8c0c4de822511bc04a7645d60c6bc7d297fdca
ClarkAlmazan <clark.almazan@gmail.com>
2017-07-20 12:56:17 +0800

    Clear views' __init__.py and add documentation for drone module

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,12 +1,3 @@
-# from app import app, login_manager, bcrypt, db, models
-# from flask import render_template, flash, redirect, session, url_for, request, g, abort, Blueprint
-# from ..forms import *
-# from flask_login import login_user, logout_user, current_user, login_required
-# from werkzeug.utils import secure_filename
-# from datetime import datetime
-# import os
-
-
-
-
-
+"""
+	Views module containing submodules for different entities.
+"""
