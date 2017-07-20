8731345acaf15692d3897cbb5c08fa105bda5dcf
ClarkAlmazan <clark.almazan@gmail.com>
2017-07-20 11:32:37 +0800

    Flight module documentation

--- app/views/flight.py
+++ app/views/flight.py
@@ -1,18 +1,17 @@
 """
         View module for Flights. This module defines routes and logic for Flight-related things.
 """
-
+import os
+import json
+from datetime import datetime
 from app import app, db #still needs app for config
-from flask import render_template, redirect, url_for, request, abort, Blueprint
+from flask import render_template, redirect, url_for, abort, Blueprint
+from app.views.log import create_map, write_log_gps_file, write_bin_gps_file, write_dronekit_la_output_file
+from flask_login import  current_user, login_required
+from werkzeug.utils import secure_filename
+
 from ..forms import FlightForm, LogForm
 from ..models import Project, Drone, Flight, Log
-from log import get_map_markers, get_first_point, get_map_markers_json, create_map, write_log_gps_file, write_bin_gps_file, write_dronekit_la_output_file, write_dronekit_la_output_file_plain
-from flask_login import  current_user, login_required
-from flask_googlemaps import Map
-from werkzeug.utils import secure_filename
-from datetime import datetime
-import os, subprocess, json
-
 
 #####   FLIGHT MANAGEMENT ROUTES AND VIEWS
 flight = Blueprint('flight', __name__)
@@ -21,10 +20,14 @@ flight = Blueprint('flight', __name__)
 @flight.route('/flights')
 @login_required
 def view_all_flights():
+    """
+        Route for viewing all of the user's flights. Automatically called when user opens '/flights'
+        in a web browser. Requires user login.
+    """
     user = current_user
-    
+
     #get projects of user
-    project_ids = db.session.query(Project.id).filter_by(user_id = user.id)
+    project_ids = db.session.query(Project.id).filter_by(user_id=user.id)
 
     #get flights from projects
     flights = Flight.query.filter(Flight.project_id.in_(project_ids)).all()
@@ -37,46 +40,56 @@ def view_all_flights():
     for flight in flights:
         flight.drone = Drone.query.get(flight.drone_id)
 
-    flight_count=len(flights)
-    return render_template("flights.html", title="Flights", user=user, flights=flights, flight_count = flight_count)
+    flight_count = len(flights)
+    return render_template("flights.html", title="Flights", user=user, flights=flights, flight_count=flight_count)
 
 #VIEW FLIGHT DETAILS
 @flight.route('/flight/view/<flight_id>', methods=['GET','POST'])
 @login_required
 def view_flight(flight_id):
+    """
+        Route for viewing more details for a user-specified flight.
+        Called when user opens '/flights/view/<flight_id>in a web browser.
+        <flight_id> is a parameter for specifying which flight to open.
+        Requires user login. Only the owner of the flight can view his/her flight. Will return
+        404 Not Found for non-existent flight, or non-matching user credentials.
+    """
     user = current_user
+
+    #check if flight exists
     flight = Flight.query.get(flight_id)
-    
     if flight is None:
         abort(404)
-    
+
+    #check if User owns the flight
     project = Project.query.get(flight.project_id)
-    
     if current_user.id != project.user_id:
         abort(404)
-    
 
     drone = Drone.query.get(flight.drone_id)
     logs = Log.query.filter_by(flight_id=flight_id).all()
     maps = []
     for log in logs:
+        #load json into a dictionary
         log.processed_content = json.loads(log.content)
         log.processed_content['timestamp'] = datetime.fromtimestamp(log.processed_content['timestamp']/1000000)
 
+        #some workaround to make maps work.
         maps.append(create_map(log.id))
         log.map = maps[len(maps)-1]
-        
-        print "Log " + log.filename + " successfully processed"     
 
+        print "Log " + log.filename + " successfully processed"
 
+    #Initialize form for adding logs
     form = LogForm()
 
     if form.validate_on_submit():
+        #get file handle and generate secure filename
         f = form.log_file.data
-        filename = secure_filename(user.username + ' - ' + str(datetime.now()) +' - '+ f.filename )
-        
+        filename = secure_filename(user.username + ' - ' + str(datetime.now()) +' - '+ f.filename)
+
         # attempt to get gps coordinates when uploaded file is a text dump (*.log)
-        gps_name = filename.rsplit('.', 1) 
+        gps_name = filename.rsplit('.', 1)
 
         # if file is a text dump, get all lines about "GPS", else no gps file
         if gps_name[1].lower() == "log":
@@ -86,7 +99,7 @@ def view_flight(flight_id):
 
         # save file
         f.save(os.path.join(app.config['ORIGINAL_LOG_FILE_FOLDER'], filename))
-        
+
         #check if file is a binary log. This check can only happen after the file has been written.
         if gps_name[1].lower() == "bin":
             gps_filename = gps_name[0] + '.csv'
@@ -97,29 +110,39 @@ def view_flight(flight_id):
         content = write_dronekit_la_output_file(filename)
 
         #save contents to db to avoid running dronekit-la again
-        log = Log(filename, content, gps_filename, processed_filename,flight_id)
-        
+        log = Log(filename, content, gps_filename, processed_filename, flight_id)
 
+        #Commit to db and refresh page
         db.session.add(log)
         db.session.commit()
         print 'Upload Successful for file: ' + filename
         return redirect(url_for('flight.view_flight', flight_id=flight_id))
 
-    return render_template('view_flight_hardcoded.html', title=flight.name,  flight=flight, drone=drone, project=project, form=form, logs=logs, maps=maps)
+    return render_template('view_flight_hardcoded.html', title=flight.name, flight=flight, drone=drone, project=project, form=form, logs=logs, maps=maps)
 
 #   ADD A FLIGHT
-@flight.route('/flight/add', methods=['GET','POST'])
+@flight.route('/flight/add', methods=['GET', 'POST'])
 @login_required
 def add_flight():
+    """
+        Route for adding flights. Called when user opens '/flight/add' in a web browser.
+        Requires user login.
+    """
     user = current_user
-    projects = db.session.query(Project.id, Project.name).filter_by(user_id = user.id)
-    drones = db.session.query(Drone.id, Drone.name).filter_by(user_id = user.id)
-    form= FlightForm()
-    form.project.choices= projects
+    #Get projects and drones of user since these are required.
+    projects = db.session.query(Project.id, Project.name).filter_by(user_id=user.id)
+    drones = db.session.query(Drone.id, Drone.name).filter_by(user_id=user.id)
+
+    #Initialize form and add choices for project and drone
+    form = FlightForm()
+    form.project.choices = projects
     form.drone.choices = drones
 
     if form.validate_on_submit():
-        duration = (form.duration_hours.data*60*60)+(form.duration_mins.data*60)+(form.duration_secs.data)
+        #Format duration to seconds only
+        duration = (form.duration_hours.data*60*60) + (form.duration_mins.data*60) + (form.duration_secs.data)
+
+        #Initialize flight object
         flight = Flight(
             form.name.data,
             form.location.data,
@@ -138,8 +161,7 @@ def add_flight():
             form.project.data
         )
 
-        print flight
-
+        #commit to db and return to view all flights page
         db.session.add(flight)
         db.session.commit()
         return redirect(url_for("flight.view_all_flights"))
@@ -147,29 +169,42 @@ def add_flight():
     return render_template("flight_form.html", title="Add Flight", form_title="Add Flight", submit_value="Add Flight", name="add_flight", projects=projects, form=form)
 
 #   EDIT FLIGHT DETAILS
-@flight.route('/flight/edit/<flight_id>', methods=['GET','POST'])
+@flight.route('/flight/edit/<flight_id>', methods=['GET', 'POST'])
 @login_required
 def edit_flight(flight_id):
+    """
+        Route for updating flight data. Called when user opens 'flight/edit/<flight_id>' in a
+        browser window. <flight_id> is a parameter that specifies which flight to update.
+        Requires user login. Returns 404 Not Found for non-existing flight, or non-matching user
+        credentials.
+    """
+
     user = current_user
+
+    #Check if flight exists
     flight = Flight.query.get(flight_id)
     if flight is None:
         abort(404)
 
+    #Check if User owns the flight
     project = Project.query.get(flight.project_id)
-    
     if current_user.id != project.user_id:
         abort(404)
-    
+
+    #Else continue with edit
     else:
-        projects = db.session.query(Project.id, Project.name).filter_by(user_id = user.id)
-        drones = db.session.query(Drone.id, Drone.name).filter_by(user_id = user.id)
-        form= FlightForm(obj=flight)
+        #Render form with preset values, and add choices for project and drone
+        projects = db.session.query(Project.id, Project.name).filter_by(user_id=user.id)
+        drones = db.session.query(Drone.id, Drone.name).filter_by(user_id=user.id)
+        form = FlightForm(obj=flight)
+        form.project.choices = projects
+        form.drone.choices = drones
+
+        #Format duration from seconds to hours, minutes, seconds
         form.duration_hours.data = flight.duration/ 3600
         form.duration_mins.data = (flight.duration % 3600) / 60
         form.duration_secs.data = flight.duration % 60
-        form.project.choices= projects
-        form.drone.choices = drones
-        
+
         if form.validate_on_submit():
             duration = (form.duration_hours.data*60*60)+(form.duration_mins.data*60)+(form.duration_secs.data)
             flight.name = form.name.data
@@ -185,9 +220,8 @@ def edit_flight(flight_id):
             flight.notes = form.notes.data
             flight.weather_description = form.weather_description.data
             flight.drone_id = form.drone.data
-            flight.project_id =  form.project.data
-            
-            print flight
+            flight.project_id = form.project.data
+
 
             db.session.commit()
             return redirect(url_for("flight.view_all_flights"))
@@ -197,15 +231,24 @@ def edit_flight(flight_id):
 @flight.route('/flight/delete/<flight_id>')
 @login_required
 def delete_flight(flight_id):
+    """
+        Route for deleting flights. Called when user opens '/flight/delete/<flight_id>' in browser
+        window. <flight_id> specifies which flight to delete.
+        Requires user login. Returns 404 Not Found for non-existent flight or non-matching user
+        credentials.
+    """
+
+    #Check if flight exists
     flight = Flight.query.get(flight_id)
     if flight is None:
         abort(404)
-    
+
+    #check if User owns the flight.
     project = Project.query.get(flight.project_id)
-    
     if current_user.id != project.user_id:
         abort(404)
-    
+
+    #else continue with delete
     else:
         db.session.delete(flight)
         db.session.commit()
