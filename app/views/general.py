4b05d4045196ffc615ada68cef11db8547b749e9
ClarkAlmazan <clark.almazan@gmail.com>
2017-07-20 13:26:43 +0800

    Documentation for general module(home, about).

--- app/views/general.py
+++ app/views/general.py
@@ -1,12 +1,21 @@
+"""
+    Views module for things that aren't classifiable to specific entities.
+"""
 from flask import Blueprint, render_template
-from flask_login import current_user
+
 
 general = Blueprint('general', __name__)
 
 @general.route('/')
 def home():
-	return render_template('home.html')
+    """
+        Route for the index/home page.
+    """
+    return render_template('home.html')
 
 @general.route('/about')
 def about():
+    """
+        Route for the About page.
+    """
     return render_template('about.html')
